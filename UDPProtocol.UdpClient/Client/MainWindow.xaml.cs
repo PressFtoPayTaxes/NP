e8a37b42ac64e2429bfa523b1142845cd86b19a0
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-06-26 20:48:46 +0600

    Init

---
+++ UDPProtocol.UdpClient/Client/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _client = new UdpClient(54321);
+        }
+
+        private UdpClient _client;
+
+        private async void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (_client == null)
+                _client = new UdpClient(54321);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            byte[] buffer = Encoding.UTF8.GetBytes("GETSCREENSHOT");
+            await _client.SendAsync(buffer, buffer.Length);
+            var receiveResult = await _client.ReceiveAsync();
+            buffer = receiveResult.Buffer;
+
+            using (FileStream stream = new FileStream("ServerScreenshot.jpg", FileMode.OpenOrCreate))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(buffer);
+                }
+            }
+
+            ImageSourceConverter converter = new ImageSourceConverter();
+            screenshotImage.Source = (ImageSource)converter.ConvertFromString("ServerScreenshot.jpg");
+
+            _client.Close();
+        }
+    }
+}
---
+++ UDPProtocol.UdpClient/Server/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 12345;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, port);
+            UdpClient client = new UdpClient(endPoint);
+
+            while (true)
+            {
+                try
+                {
+                    // Тут возникает непонятная ошибка с переполнением буфера или очереди, с которой я так
+                    //  и не смог разобраться
+                    // Остальное должно работать
+                    client.BeginReceive(ReceiveRequests, new UdpState { Client = client, EndPoint = endPoint });
+                }
+                catch(SocketException exception)
+                {
+                    MessageBox.Show($"Ошибка сокета({exception.ErrorCode}): {exception.Message}");
+                    return;
+                }
+            }
+        }
+
+        static void ReceiveRequests(IAsyncResult result)
+        {
+            UdpClient client = ((UdpState)result.AsyncState).Client;
+            IPEndPoint endPoint = ((UdpState)result.AsyncState).EndPoint;
+
+            byte[] buffer = client.EndReceive(result, ref endPoint);
+            string message = Encoding.UTF8.GetString(buffer);
+            if (message == "GETSCREESHOT")
+            {
+                ThreadPool.QueueUserWorkItem(SendScreenshot, new UdpState { Client = client, EndPoint = endPoint });
+            }
+        }
+
+        private static void SendScreenshot(object state)
+        {
+            IPEndPoint endPoint = (state as UdpState).EndPoint;
+            UdpClient client = (state as UdpState).Client;
+
+            var bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (var graphic = Graphics.FromImage(bitmap))
+            {
+                graphic.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            }
+            bitmap.Save("Screenshot.jpg");
+
+            using (var stream = new FileStream("Screenshot.jpg", FileMode.Open))
+            {
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    byte[] buffer = reader.ReadBytes((int)stream.Length);
+                    client.Send(buffer, (int)stream.Length, endPoint);
+                }
+            }
+        }
+    }
+}
