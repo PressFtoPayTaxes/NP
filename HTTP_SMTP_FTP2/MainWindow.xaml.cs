7c5fc5f2d6b341b6f6c571031aa509b19df4654a
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-06-28 18:12:04 +0600

    Init

---
+++ HTTP_SMTP_FTP2/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace HTTP_SMTP_FTP2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectPathButtonClick(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.ShowDialog();
+
+            pathTextBox.Text = dialog.FileName;
+        }
+
+        private void DownloadButtonClick(object sender, RoutedEventArgs e)
+        {
+            downloadButton.IsEnabled = false;
+            downloadButton.Content = "Скачивается...";
+
+            ThreadPool.QueueUserWorkItem(DownloadFile);
+        }
+
+        private void DownloadFile(object state)
+        {
+            string url = "";
+            string path = "";
+            Dispatcher.Invoke(() => url = urlTextBox.Text);
+            Dispatcher.Invoke(() => path = pathTextBox.Text);
+
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(url, path);
+                }
+            }
+            catch(WebException exception)
+            {
+                MessageBox.Show($"Ошибка сети: {exception.Message}");
+                Dispatcher.Invoke(() =>
+                {
+                    downloadButton.IsEnabled = true;
+                    downloadButton.Content = "Скачать";
+                });
+                return;
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show($"Ошибка: {exception.Message}");
+                Dispatcher.Invoke(() =>
+                {
+                    downloadButton.IsEnabled = true;
+                    downloadButton.Content = "Скачать";
+                });
+                return;
+            }
+
+            MessageBox.Show("Файл скачан");
+            Dispatcher.Invoke(() =>
+            {
+                downloadButton.IsEnabled = true;
+                downloadButton.Content = "Скачать";
+            });
+        }
+    }
+}
